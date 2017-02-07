09a1ec134b5853ae3845ace1e059629e063e71c1
jingzhixiang <jingzhixiang@aprbrother.com>
2017-02-07 17:44:21 +0800

    add  dub file

--- main.d
+++ main.d
@@ -4,13 +4,5 @@ import std.process;
 
 void main()
 {
-	ubyte count=0;
-	dchar[] name;
-	int number;
-    int grade;
-	int[] arr=[1,2,3,4];
-	writeln(arr);
-	foreach(i;arr){
-		writeln(i);
-	}
+	writefln("runDlang");
 }
